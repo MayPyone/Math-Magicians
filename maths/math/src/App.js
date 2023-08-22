a839de8eef6ba2649d1e3f7a32ab418cf0a09b19
May Pyone <maypyone015@gmail.com>
2023-08-22 23:16:17 +0630

    Fix bugs

--- maths/math/src/App.js
+++ maths/math/src/App.js
@@ -3,36 +3,36 @@ import './index.css';
 function App() {
   return (
     <>
-    <form>
-      <div><input type="text" /></div>
-      <div className='buttons'>
-        <button>Ac</button>
-        <button>+/-</button>
-        <button>%</button>
-        <button>&divide;</button>
+      <form>
+        <div><input type="text" /></div>
+        <div className="buttons">
+          <button type="button">Ac</button>
+          <button type="button">+/-</button>
+          <button type="button">%</button>
+          <button type="button">&divide;</button>
 
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>&times;</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">&times;</button>
 
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">-</button>
 
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button">+</button>
         </div>
-        <div className='last'>
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
+        <div className="last">
+          <button type="button">0</button>
+          <button type="button">.</button>
+          <button type="button">=</button>
         </div>
-    </form>
-  </>
+      </form>
+    </>
   );
 }
 
