b6ad89d8f38229fe207185838d39650eb3458453
May Pyone <maypyone015@gmail.com>
2023-08-24 00:01:19 +0630

    Implement the UI

--- src/compnents/Calculator.js
+++ src/compnents/Calculator.js
@@ -9,8 +9,8 @@ export function Calculate() {
 
 function UserInput() {
   return (
-    <div>
-      <input type="text" />
+    <div className="result">
+      0
     </div>
   );
 }
@@ -24,27 +24,27 @@ export function ButtonGroup() {
           <button type="button">Ac</button>
           <button type="button">+/-</button>
           <button type="button">%</button>
-          <button type="button">&divide;</button>
+          <button type="button" className="b-orange">&divide;</button>
 
           <button type="button">7</button>
           <button type="button">8</button>
           <button type="button">9</button>
-          <button type="button">&times;</button>
+          <button type="button" className="b-orange">&times;</button>
 
           <button type="button">4</button>
           <button type="button">5</button>
           <button type="button">6</button>
-          <button type="button">-</button>
+          <button type="button" className="b-orange">-</button>
 
           <button type="button">1</button>
           <button type="button">2</button>
           <button type="button">3</button>
-          <button type="button">+</button>
+          <button type="button" className="b-orange">+</button>
         </div>
         <div className="last">
           <button type="button">0</button>
           <button type="button">.</button>
-          <button type="button">=</button>
+          <button type="button" className="b-orange">=</button>
         </div>
       </form>
     </>
