ae3718f5ce53592c4c23f11fbc6cebd62e012c91
May Pyone <maypyone015@gmail.com>
2023-08-30 22:44:20 +0630

    Update files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Testing included links', () => {
+  const { getByText } = render(
+      <App />
+  );
+
+  const homeLink = getByText('Home');
+  const calculatorLink = getByText('Calculator');
+  const quotesLink = getByText('Quotes');
+
+  expect(homeLink).toBeInTheDocument();
+  expect(calculatorLink).toBeInTheDocument();
+  expect(quotesLink).toBeInTheDocument();
 });
